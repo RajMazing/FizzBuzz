0427add72c5a669415153c06698699a2988988d5
RajMazing <caseyraj92@gmail.com>
2022-09-07 15:24:53 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+
+        if (number % 5 == 0)
+            System.out.println("Fizz");
+    }
+}
