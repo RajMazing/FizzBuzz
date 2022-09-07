ab54a1754b5e72287ecda0ad9170b703c0da0daa
RajMazing <caseyraj92@gmail.com>
2022-09-07 15:47:32 -0500

    Working example on this branch

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -6,5 +8,11 @@ public class Main {
 
         if (number % 5 == 0)
             System.out.println("Fizz");
+        else if (number % 3 == 0)
+            System.out.println("Buzz");
+        else if (number % 5 == 0 &&  number % 3  == 0)
+            System.out.println("FizzBuzz");
+        else
+            System.out.println(number);
     }
 }
